0a288cc101cefeb5ee933b6df70c758d5be3116d
TobbeTripitaka <tobbe@tripitaka.se>
2015-10-12 01:19:23 +0200

    Update plot_2.R

--- plot_2.R
+++ plot_2.R
@@ -4,6 +4,6 @@ source('explore1.R')
 
 png(filename='plot2.png',width=480,height=480,units='px')
 
-plot(power.consumption$DateTime,power.consumption$GlobalActivePower,ylab='Global Active Power (kilowatts)', xlab='', type='l')
+plot(power.consumption$DateTime,power.consumption$GlobalActivePower,ylab='Global Active Power (kW)', xlab='', type='l')
 
 x<-dev.off()
