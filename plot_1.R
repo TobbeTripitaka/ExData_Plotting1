88eba7a712c9b8a491d0df432a486fc55b9c2d12
TobbeTripitaka <tobbe@tripitaka.se>
2015-10-12 01:18:05 +0200

    Update plot_1.R

--- plot_1.R
+++ plot_1.R
@@ -4,7 +4,6 @@ source('explore1.R')
 
 png(filename='plot1.png',width=480,height=480,units='px')
 
-# plot data
 hist(power.consumption$GlobalActivePower,main='Global Active Power',xlab='Global Active Power (kilowatts)',col='red')
 
 x<-dev.off()
